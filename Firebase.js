8b4928c6bec87eed7716a934fb63df8a09a7a9e7
nao-romasaga <47514993+nao-romasaga@users.noreply.github.com>
2019-03-30 13:58:51 +0900

    Create Firebase.js

---
+++ Firebase.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCKpg76hjQg4YNSW3hGEw5uCJOBbQNUsnQ",
+    authDomain: "nao-romasaga-rs.firebaseapp.com",
+    databaseURL: "https://nao-romasaga-rs.firebaseio.com",
+    projectId: "nao-romasaga-rs",
+    storageBucket: "nao-romasaga-rs.appspot.com",
+    messagingSenderId: "707644829042"
+};
+
+firebase.initializeApp(config);
