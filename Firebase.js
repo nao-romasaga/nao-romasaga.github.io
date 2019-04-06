4b5167b4d079ee19337a6d3cd9d7e3371d50aacb
nao-romasaga <47514993+nao-romasaga@users.noreply.github.com>
2019-04-07 01:47:43 +0900

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,7 +1,10 @@
 var config = {
     apiKey: "AIzaSyCKpg76hjQg4YNSW3hGEw5uCJOBbQNUsnQ",
+    //authDomain: "nao-romasaga-rs.firebaseapp.com",
     databaseURL: "https://nao-romasaga-rs.firebaseio.com",
+    //projectId: "nao-romasaga-rs",
     storageBucket: "nao-romasaga-rs.appspot.com",
+    //messagingSenderId: "707644829042"
 };
 
 firebase.initializeApp(config);
@@ -9,6 +12,7 @@ firebase.auth().signInAnonymously().catch(function (error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
+    console.log(errorCode, errorMessage);
     // ...
 });
 firebase.auth().onAuthStateChanged(function (user) {
@@ -16,6 +20,7 @@ firebase.auth().onAuthStateChanged(function (user) {
         // User is signed in.
         var isAnonymous = user.isAnonymous;
         var uid = user.uid;
+        console.log(user, isAnonymous, uid);
         // ...
     } else {
         console.log("signed out");
@@ -28,6 +33,7 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 function showimage(path, id, size) {
     firebase.storage().ref().child(path).getDownloadURL().then(function (url) {
+        //console.log(url);
         $("#" + id).css("background", "url(" + url + ") no-repeat");
         if (size !== undefined) {
             $("#" + id).css("background-size", size);
@@ -37,9 +43,17 @@ function showimage(path, id, size) {
     });
 }
 
-async function setImgTag(path, id) {
+function setImgTag(path, id) {
     firebase.storage().ref().child(path).getDownloadURL().then(function (url) {
         $("#" + id).attr("src", url);
+        return url;
+    }).catch(function (error) {
+
+    });
+}
+function setImgClassTag(path, id) {
+    firebase.storage().ref().child(path).getDownloadURL().then(function (url) {
+        $("." + id).attr("src", url);
         return url;
     }).catch(function (error) {
 
