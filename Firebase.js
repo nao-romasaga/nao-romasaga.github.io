c0ab3090f6e520688ca57adc87acffdc1274ea0a
nao-romasaga <47514993+nao-romasaga@users.noreply.github.com>
2019-04-09 02:37:13 +0900

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,10 +1,7 @@
 var config = {
     apiKey: "AIzaSyCKpg76hjQg4YNSW3hGEw5uCJOBbQNUsnQ",
-    //authDomain: "nao-romasaga-rs.firebaseapp.com",
     databaseURL: "https://nao-romasaga-rs.firebaseio.com",
-    //projectId: "nao-romasaga-rs",
     storageBucket: "nao-romasaga-rs.appspot.com",
-    //messagingSenderId: "707644829042"
 };
 
 firebase.initializeApp(config);
@@ -12,7 +9,6 @@ firebase.auth().signInAnonymously().catch(function (error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    console.log(errorCode, errorMessage);
     // ...
 });
 firebase.auth().onAuthStateChanged(function (user) {
@@ -20,10 +16,8 @@ firebase.auth().onAuthStateChanged(function (user) {
         // User is signed in.
         var isAnonymous = user.isAnonymous;
         var uid = user.uid;
-        console.log(user, isAnonymous, uid);
         // ...
     } else {
-        console.log("signed out");
         // User is signed out.
         // ...
     }
