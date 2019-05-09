1ebc8b712b598cad780c77534629de67183c348f
nao-romasaga <47514993+nao-romasaga@users.noreply.github.com>
2019-05-09 15:00:11 +0900

    Update Firebase_sp.js

--- js/Firebase_sp.js
+++ js/Firebase_sp.js
@@ -4,37 +4,56 @@ var config = {
     storageBucket: "nao-romasaga-rs-dev.appspot.com",
 };
 
-firebase.initializeApp(config);
-firebase.auth().signInAnonymously().catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-});
-firebase.auth().onAuthStateChanged(function (user) {
+var REF;
+var UID;
+firebase.auth(appUsers).onAuthStateChanged((user) => {
     if (user) {
-        // User is signed in.
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        // ...
+        UID = user.uid;
+        REF = firebase.database(appUsers).ref('user_data/' + UID);
+        $(".RequireLoginMenu").removeClass("d-none");
     } else {
-        // User is signed out.
-        // ...
+        console.log("no login");
     }
-    // ...
+        console.log(UID);
 });
-
 function readFile(target, callback) {
-        console.log(target + " start");
-    return firebase.database().ref(target).once("value").then(function (snapshot) {
-        console.log(target);
-        return callback(snapshot.val());
+    return firebase.database().ref(`game_data`).once("value").then(function (snapshot) {
+        return callback(snapshot.val()[target]);
+    });
+}
+function readAnalyzeFile(target, callback) {
+    return firebase.database().ref(`analyze_data`).once("value").then(function (snapshot) {
+        return callback(snapshot.val()[target]);
     });
 }
 
-function getImgUrl(target) {
-    let url = "https://nao-romasaga.github.io/img/" + target;
-    return 'background:url(' + url + ') no-repeat;'
+
+function readCharData(charId, callback) {
+    return firebase.database(appUsers).ref(`user_data/${UID}/CHAR/${charId}`).once("value").then(function (snapshot) {
+        return callback(snapshot.val());
+    });
+}
+function readPartyData(callback) {
+    console.log(`user_data/${UID}/PARTY`);
+    return firebase.database(appUsers).ref(`user_data/${UID}/PARTY`).once("value").then(function (snapshot) {
+        return callback(snapshot.val());
+    });
+}
+function readStyleCheckData(callback) {
+    return firebase.database(appUsers).ref(`user_data/${UID}/STYLECHECK`).once("value").then(function (snapshot) {
+        return callback(snapshot.val());
+    });
+}
+function updateData(key, data) {
+    if (REF !== undefined) {
+        REF = firebase.database(appUsers).ref(`user_data/${UID}/${key}`);
+        REF.update(data);
+    }
 }
 
 
+function getImgUrl(target) {
+    //let url = "http://localhost/img/" + target;
+    let url = "https://nao-romasaga.github.io/img/" + target;
+    return 'background:url(' + url + ') no-repeat;'
+}
