1097e3c18b81827939b5c3051e6578f8704f28dc
nao-romasaga <47514993+nao-romasaga@users.noreply.github.com>
2019-05-15 11:15:04 +0900

    Update Firebase.js

--- js/Firebase.js
+++ js/Firebase.js
@@ -20,7 +20,6 @@ firebase.auth(appUsers).onAuthStateChanged((user) => {
         UID = user.uid;
         REF = firebase.database(appUsers).ref('user_data/' + UID);
         $(".RequireLoginMenu").removeClass("d-none");
-        $("#infoCard").html('');        
     } else {
         console.log("no login");
     }
